fa4eee9e8668b3d80825051f581bb34092c498ce
Nakimi <robinhubert02@gmail.com>
2025-03-19 17:57:09 +0100

    ajout livres et auteurs

--- server.js
+++ server.js
@@ -1,48 +1,21 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import express, { json } from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import itemsRouter from "./src/routers/itemsRouters.js";
+import auteursRouter from "./src/routers/auteursRouters.js";
+import livresRouter from "./src/routers/livresRouters.js";
+import PORT from "./config/config.js";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
-// Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connecté"))
-  .catch(err => console.error(err));
+connectDB();
 
-// Modèle Item
-const ItemSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-});
+// ROUTES
+app.get("/", (req, res) => { res.send("API en ligne 🚀"); });
+app.use("/items", itemsRouter);
+app.use("/auteurs", auteursRouter);
+app.use("/livres", livresRouter);
 
-const Item = mongoose.model("Item", ItemSchema);
-
-// Routes API
-app.get("/", (req, res) => {
-  res.send("API en ligne 🚀");
-});
-
-app.get("/items", async (req, res) => {
-  const items = await Item.find();
-  res.json(items);
-});
-
-app.post("/items", async (req, res) => {
-  const newItem = new Item(req.body);
-  await newItem.save();
-  res.json(newItem);
-});
-
-app.delete("/items/:id", async (req, res) => {
-  await Item.findByIdAndDelete(req.params.id);
-  res.json({ message: "Item supprimé" });
-});
-
-// Lancer le serveur
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Serveur sur http://localhost:${PORT}`));
